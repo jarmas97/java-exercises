480864a5740a4bc99466f55c1e85e02adbb5cd6f
jarmas97 <jerry.maslowski97@gmail.com>
2022-06-27 12:02:29 +0200

    QuadraticEquation implemented

--- src/main/java/QuadraticEquation.java
+++ src/main/java/QuadraticEquation.java
@@ -3,9 +3,19 @@
 // If the equation has only one solution, the function should return that solution as both elements of the Roots.
 // The equation will always have at least one solution.
 
+import static java.lang.StrictMath.sqrt;
+
 public class QuadraticEquation {
     public static Roots findRoots(double a, double b, double c) {
-        throw new UnsupportedOperationException("Waiting to be implemented.");
+        double delta = (b * b) -1 *(4 * a * c);
+        if (delta >= 0) {
+
+            double x1 = (-b -sqrt(delta)) / (2 * a);
+            double x2 = (-b +sqrt(delta)) / (2 * a);
+            return new Roots(x1, x2);
+        } else {
+            return new Roots(0, 0);
+        }
     }
 
     public static void main(String[] args) {
