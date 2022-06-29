9c41e96b9bdb940fed875704dd8b11d5c102b1ae
jarmas97 <jerry.maslowski97@gmail.com>
2022-06-29 22:39:00 +0200

    Stream tasks
    Exercise 2 is DONE

--- src/main/java/streams/Main.java
+++ src/main/java/streams/Main.java
@@ -1,4 +1,7 @@
 package streams;
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,23 +10,65 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Product bookTroy240 = new Product((long) 1, "Troy", "Books", 240.0);
+        Product book300290 = new Product((long) 2, "300", "Books", 290.0);
+        Product bookHarryPotter89 = new Product((long) 3, "Harry Potter", "books", 89.0);
+        Product puzzleHarryPotter110 = new Product((long) 4, "Harry Potter:Puzzle", "Puzzles", 110.0);
+
+        List<Product> products0 = new ArrayList<>();
+        products0.add(bookTroy240);
+        products0.add(book300290);
+        products0.add(bookHarryPotter89);
+        products0.add(puzzleHarryPotter110);
+
+        Product babyToys45 = new Product((long) 5, "Toys", "baby", 45.0);
+        Product babyPinkClothes35 = new Product((long) 6, "PinkClothes", "baby", 35.0);
+        Product babyBoots97 = new Product((long) 7, "Boots", "baby", 97.0);
+
+        List<Product> products1 = new ArrayList<>();
+        products1.add(babyToys45);
+        products1.add(babyPinkClothes35);
+        products1.add(babyBoots97);
+
+        Order order0 = new Order(
+                (long) 1,
+                LocalDate.of(2022, 6, 20),
+                LocalDate.of(2022, 6, 29),
+                "DONE", products0
+        );
+        Order order1 = new Order(
+                (long) 2,
+                LocalDate.of(2022, 6, 21),
+                LocalDate.of(2022, 6, 29),
+                "DONE", products1
+        );
+
+        List<Order> orders0 = new ArrayList<>();
+        orders0.add(order0);
+        orders0.add(order1);
+
+
 //      Exercise 1 — Obtain a list of products belongs to category “Books” with price > 100
 
-        List<Product> productList = new ArrayList<>();
-        productList.add(new Product((long) 1, "Troy", "Books", 240.0));
-        productList.add(new Product((long) 2, "300", "Books", 290.0));
-        productList.add(new Product((long) 3, "Harry Potter", "books", 89.0));
-        productList.add(new Product((long) 4, "Harry Potter:Puzzle", "Puzzles", 110.0));
-
-        List<Product> result = productList.stream().filter(p -> p.getCategory().equalsIgnoreCase("Books"))
+        List<Product> result0 = products0.stream().filter(p -> p.getCategory().equalsIgnoreCase("Books"))
                 .filter(product -> product.getPrice() > 100)
                 .collect(Collectors.toList());
-        for (Product product : result) {
+        for (Product product : result0) {
             System.out.println(product);
         }
 
 //      Exercise 2 — Obtain a list of order with products belong to category “Baby”
 
+        List<Order> result1 = orders0
+                .stream()
+                .filter(order -> order.getProductList()
+                        .stream()
+                        .anyMatch(product -> product.getCategory().equalsIgnoreCase("baby"))
+                )
+                .collect(Collectors.toList());
+        for (Order order : result1) {
+            System.out.println(order.getProductList());
+        }
 
     }
 }
--- src/main/java/streams/Order.java
+++ src/main/java/streams/Order.java
@@ -1,6 +1,8 @@
 package streams;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Order {
@@ -8,30 +10,30 @@ public class Order {
     private LocalDate orderDate;
     private LocalDate deliveryDate;
     private String status;
+    private List<Product> productList = new ArrayList<>();
 
-    public Order(Long id, LocalDate orderDate, LocalDate deliveryDate, String status) {
+    public Order(Long id, LocalDate orderDate, LocalDate deliveryDate, String status, List<Product> productList) {
         this.id = id;
         this.orderDate = orderDate;
         this.deliveryDate = deliveryDate;
         this.status = status;
+        this.productList = productList;
     }
-
     public Long getId() {
         return id;
     }
-
     public LocalDate getOrderDate() {
         return orderDate;
     }
-
     public LocalDate getDeliveryDate() {
         return deliveryDate;
     }
-
     public String getStatus() {
         return status;
     }
-
+    public List<Product> getProductList() {
+        return productList;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -40,11 +42,11 @@ public class Order {
         return Objects.equals(id, order.id) &&
                 Objects.equals(orderDate, order.orderDate) &&
                 Objects.equals(deliveryDate, order.deliveryDate) &&
-                Objects.equals(status, order.status);
+                Objects.equals(status, order.status) &&
+                Objects.equals(productList, order.productList);
     }
-
     @Override
     public int hashCode() {
-        return Objects.hash(id, orderDate, deliveryDate, status);
+        return Objects.hash(id, orderDate, deliveryDate, status, productList);
     }
 }
